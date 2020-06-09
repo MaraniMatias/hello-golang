437a0ee72d6484568a769a7f8f6caabda1603684
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-06-08 21:46:36 -0300

    Options parameters

--- main.go
+++ main.go
@@ -14,13 +14,34 @@ func Person(name string, age int) *person {
 	return &p
 }
 
+const english = "English"
+const spanish = "Spanish"
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name string, language ...string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	var l = english
+	if len(language) == 1 {
+		l = language[0]
+	}
+	return greetingPrefix(l) + name
 }
 
 func main() {
